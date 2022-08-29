e407d909a15ca489cbdf74d529ce7328d69e2ed9
Mend1s <andremendesskt99@gmail.com>
2022-08-29 14:19:43 -0300

    aula 4 - acesso ao bd com ef

--- DevTrackr.API/Persistence/DevTrackRContext.cs
+++ DevTrackr.API/Persistence/DevTrackRContext.cs
@@ -1,6 +1,36 @@
-﻿namespace DevTrackr.API.Persistence
+﻿using DevTrackr.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevTrackr.API.Persistence
 {
-    public class DevTrackRContext
+    public class DevTrackRContext : DbContext
     {
+        public DevTrackRContext(DbContextOptions<DevTrackRContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<PackageUpdate> PackageUpdates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Package>(e =>
+            {
+                e.HasKey(p => p.Id);
+
+                e
+                .HasMany(p => p.Updates)
+                .WithOne()
+                .HasForeignKey(pu => pu.PackageId)
+                .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            builder.Entity<PackageUpdate>(e =>
+            {
+                e.HasKey(p => p.Id);
+            });
+        }   
     }
 }
--- DevTrackr.API/Program.cs
+++ DevTrackr.API/Program.cs
@@ -1,6 +1,15 @@
+using DevTrackr.API.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DevTrackRContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevTrackRCs"));
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
